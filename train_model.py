84d3a793c2142f455db1976b4bce893a3d097564
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-04-19 13:23:43 +0530

    25 epochs

--- train_model.py
+++ train_model.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import json
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+import numpy as np
 
 # Directory setup
 train_dir = './train'
@@ -30,14 +33,37 @@ test_data = test_datagen.flow_from_directory(  # Changed from train_datagen to t
 model = load_model('skin_disease_model.h5') 
 
 # Continue training
-model.fit(
+history = model.fit(
     train_data,
     validation_data=test_data,
-    epochs=5  # Train for 3 more epochs
+    epochs=10
 )
+# classification report
+
+def evaluate_model_after_chunk(model, history, test_data):
+    # Plot accuracy
+    plt.plot(history.history['accuracy'], label='Train Acc')
+    plt.plot(history.history['val_accuracy'], label='Val Acc')
+    plt.title('Accuracy')
+    plt.legend()
+    plt.show()
+
+    # Plot loss
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Val Loss')
+    plt.title('Loss')
+    plt.legend()
+    plt.show()
+
+    # Classification report
+    preds = model.predict(test_data)
+    y_pred = np.argmax(preds, axis=1)
+    y_true = test_data.classes
+    labels = list(test_data.class_indices.keys())
+
+    print(classification_report(y_true, y_pred, target_names=labels))
 
 # Save
 model.save('skin_disease_model.h5')
-# # Save class labels mapping
-# with open('class_indices.json', 'w') as f:
-#     json.dump(train_data.class_indices, f)
+
+evaluate_model_after_chunk(model, history, test_data)
